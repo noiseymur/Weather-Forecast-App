db363aea59096f74c316901d665f68d4150288f3
Dj Seymur <dj.seymur@outlook.com>
2020-09-17 23:34:37 +0400

    Updates

---
+++ precache-manifest.f002cf7f73d2b145dea5c5ef60661f5a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648c4ca53eee12fe140dc924fabc652a",
+    "url": "/Weather-Forecast-App/index.html"
+  },
+  {
+    "revision": "ec155489386f5652719e",
+    "url": "/Weather-Forecast-App/static/css/main.d7a76619.chunk.css"
+  },
+  {
+    "revision": "91343741a00d6ea3d77c",
+    "url": "/Weather-Forecast-App/static/js/2.47967651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Forecast-App/static/js/2.47967651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec155489386f5652719e",
+    "url": "/Weather-Forecast-App/static/js/main.468701f6.chunk.js"
+  },
+  {
+    "revision": "5816a39ccc3c01ea7ff4",
+    "url": "/Weather-Forecast-App/static/js/runtime-main.1f00e787.js"
+  },
+  {
+    "revision": "431804e5e5f0ca4cbcb51e59cb01997f",
+    "url": "/Weather-Forecast-App/static/media/LineIcons.431804e5.eot"
+  },
+  {
+    "revision": "662f9b951411d7d4d7ce80a63c646b37",
+    "url": "/Weather-Forecast-App/static/media/LineIcons.662f9b95.woff"
+  },
+  {
+    "revision": "764a78c49babb8c4e4f70680d791c0f8",
+    "url": "/Weather-Forecast-App/static/media/LineIcons.764a78c4.ttf"
+  },
+  {
+    "revision": "ab4f93708f366ab8c6576d60e1646dbd",
+    "url": "/Weather-Forecast-App/static/media/LineIcons.ab4f9370.woff2"
+  },
+  {
+    "revision": "b15c56e44059f3eb14a8aa204396b136",
+    "url": "/Weather-Forecast-App/static/media/LineIcons.b15c56e4.svg"
+  },
+  {
+    "revision": "bd652b9ee30ed7c7feb9c7cd66e73f2e",
+    "url": "/Weather-Forecast-App/static/media/cloudy.bd652b9e.svg"
+  },
+  {
+    "revision": "7ee70f9fbfac3ec8b5360a4b53007c00",
+    "url": "/Weather-Forecast-App/static/media/foggy.7ee70f9f.svg"
+  },
+  {
+    "revision": "075febfd9bf1429d48dd9464f094cb77",
+    "url": "/Weather-Forecast-App/static/media/rainy.075febfd.svg"
+  },
+  {
+    "revision": "b3673467354aa397d42e76e43a8a56bf",
+    "url": "/Weather-Forecast-App/static/media/snowy.b3673467.svg"
+  },
+  {
+    "revision": "f50dfdb7787fe912542321d10ea5a44c",
+    "url": "/Weather-Forecast-App/static/media/stormy.f50dfdb7.svg"
+  },
+  {
+    "revision": "346b346f5561d4876d9dede130c3b66a",
+    "url": "/Weather-Forecast-App/static/media/sunny.346b346f.svg"
+  }
+]);
